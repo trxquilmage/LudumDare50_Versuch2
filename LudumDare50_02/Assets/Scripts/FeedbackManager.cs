7f608f3ef773679c2b638d8c5cdc4a68b85f855d
Maria Fleischer <mariafleischer111@gmail.com>
2022-04-03 18:15:10 +0200

    [added] nothing

--- LudumDare50_02/Assets/Scripts/FeedbackManager.cs
+++ LudumDare50_02/Assets/Scripts/FeedbackManager.cs
@@ -18,6 +18,7 @@ public class FeedbackManager : MonoBehaviour
     {
         if (startFeedback)
         {
+            Debug.Log("A");
             if (!ab)
                 InstantiateFeedbackUnderneathEachStep();
             ab = true;
